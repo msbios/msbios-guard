08ba28db74a030843fa86d2238137fbeff081d81
Judzhin Miles <info@msbios.com>
2017-11-26 00:44:00 +0200

    Lazy Composer Commiter

--- config/autoload/development.global.php
+++ config/autoload/development.global.php
@@ -3,7 +3,11 @@
  * @access protected
  * @author Judzhin Miles <info[woof-woof]msbios.com>
  */
+
 namespace MSBios\Guard;
+
+use Zend\Router\Http\Literal;
+use Zend\ServiceManager\Factory\InvokableFactory;
 
 return [
 
@@ -11,6 +15,45 @@ return [
         'dsn' => 'mysql:dbname=portal.dev;host=127.0.0.1',
         'username' => 'root',
         'password' => 'root',
+    ],
+
+    'router' => [
+        'routes' => [
+            'home' => [
+                'type' => Literal::class,
+                'options' => [
+                    'route' => '/',
+                    'defaults' => [
+                        'controller' => Controller\IndexController::class,
+                        'action' => 'index',
+                    ],
+                ],
+            ],
+        ],
+    ],
+
+    'controllers' => [
+        'factories' => [
+            Controller\IndexController::class =>
+                InvokableFactory::class,
+        ]
+    ],
+
+    'view_manager' => [
+        //'display_not_found_reason' => true,
+        //'display_exceptions' => true,
+        //'doctype' => 'HTML5',
+        //'not_found_template' => 'error/404',
+        //'exception_template' => 'error/index',
+        //'template_map' => [
+        //    // 'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
+        //    // 'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
+        //    // 'error/404'               => __DIR__ . '/../view/error/404.phtml',
+        //    // 'error/index'             => __DIR__ . '/../view/error/index.phtml',
+        //],
+        'template_path_stack' => [
+            __DIR__ . '/../../view',
+        ],
     ],
 
     \MSBios\Assetic\Module::class => [
@@ -85,7 +128,8 @@ return [
         // Keys are the provider service names, values are the options to be passed to the provider
         'resource_providers' => [
             Provider\ResourceProvider::class => [
-                'route/home'
+                'route/home',
+                Controller\IndexController::class
             ]
         ],
 
---
+++ src/Controller/IndexController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @access protected
+ * @author Judzhin Miles <info[woof-woof]msbios.com>
+ */
+namespace MSBios\Guard\Controller;
+
+use MSBios\Guard\GuardInterface;
+use Zend\Mvc\Controller\AbstractActionController;
+
+/**
+ * Class IndexController
+ * @package MSBios\Guard\Controller
+ */
+class IndexController extends AbstractActionController implements GuardInterface
+{
+
+}
